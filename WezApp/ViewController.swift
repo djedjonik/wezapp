45960027df01d116f0a81105a0f84b994c05ff01
Andrew <bold2rev@gmail.com>
2020-08-12 14:40:27 +0300

    Initial Commit

---
+++ WezApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WezApp
+//
+//  Created by Andrew Boldyrev on 12.08.2020.
+//  Copyright © 2020 Andrew Boldyrev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
